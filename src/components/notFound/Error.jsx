d47ebf80574fa3a5966b6b3ec8f5cedb00232850
Shalala <shalala.shafiyeva23@gmail.com>
2024-08-04 22:37:29 +0400

    NotFound page

--- src/components/notFound/Error.jsx
+++ src/components/notFound/Error.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import './notFound.css';
+import React from "react";
+import "./notFound.css";
+import { Link } from "react-router-dom";
 
 function Error() {
   return (
     <div className="notFound">
-        <div className="img">
-          <img src="/images/notfound.png" alt="Not Found" />
-        </div>
-        <h4>Oops! Page not found...!</h4>
-        <span>
-          Velit dolor leo amet ultricies elementum ultricies urna magna bibendum
-          enim mauris a tellus rhoncus. Sapien nisi viverra.
-        </span>
-        <a href="">
-          <span>GO TO HOME PAGE</span>
-          <img src="/images/Right.png" alt="Right Arrow" />
-        </a>
+      <div className="img">
+        <img src="/images/notfound.png" alt="Not Found" />
       </div>
-  )
+      <h4>Oops! Page not found...!</h4>
+      <span>
+        Velit dolor leo amet ultricies elementum ultricies urna magna bibendum
+        enim mauris a tellus rhoncus. Sapien nisi viverra.
+      </span>
+      <Link to="/">
+        <span>GO TO HOME PAGE</span>
+        <img src="/images/Right.png" alt="Right Arrow" />
+      </Link>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
